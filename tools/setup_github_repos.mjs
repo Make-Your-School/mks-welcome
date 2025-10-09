5ef8c93a88f11f7794f7e332b1308ce435885bdc
s-light <git@s-light.eu>
2025-10-09 08:09:36 +0200

    fix already available repo check (pagination)

--- tools/setup_github_repos.mjs
+++ tools/setup_github_repos.mjs
@@ -43,24 +43,66 @@ async function createRepoFromTemplate(octokit, name, description) {
     return result
 }
 
+async function repoSetHomepage(octokit, name, homepage) {
+    const result = await octokit.request('PATCH /repos/{owner}/{repo}', {
+        owner: 'Make-Your-School',
+        repo: name,
+        name: name,
+        homepage: homepage,
+        // description: 'This is your first repository',
+        // private: true,
+        // has_issues: true,
+        // has_projects: true,
+        // has_wiki: true,
+        // topics not allowed. skipping for now.
+        // topics: 'markdown arduino library mks make-your-school',
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28',
+        },
+    })
+    return result
+}
+
 async function listMYSRepos(octokit) {
+    const regexLinkNext = /<(?<url_next>.*?)>; rel="next"/gm
     try {
-        const response = await octokit.request('GET /users/{username}/repos', {
+        let response = await octokit.request('GET /users/{username}/repos', {
             username: 'Make-Your-School',
+            per_page: 100,
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28',
             },
         })
-        // console.log('Response Data:', response.data)
-        console.log('Response Data:')
+        // console.log('Response:', response)
+        // console.log('Response.headers:', response.headers)
+        // console.log('Response Data:')
         // for (const repo of response.data) {
         //     console.log(repo.name)
         // }
-        const repoNames = response.data.map((item) => {
+        const repoList = response.data.map((item) => {
             return { name: item.name, clone_url: item.clone_url }
         })
-        // console.log("repoNames", repoNames)
-        return repoNames
+
+        console.error('response.headers.link', response.headers.link)
+        // '<https://api.github.com/user/99975041/repos?page=2>; rel="next", <https://api.github.com/user/99975041/repos?page=3>; rel="last"'
+        let url_next = regexLinkNext.exec(response.headers.link)?.groups?.url_next
+        while (url_next) {
+            console.error('pagination aktive! we need to get the rest...')
+            console.log('url_next', url_next)
+            response = await octokit.request(url_next, {
+                headers: {
+                    'X-GitHub-Api-Version': '2022-11-28',
+                },
+            })
+            const repoListTemp = response.data.map((item) => {
+                return { name: item.name, clone_url: item.clone_url }
+            })
+            repoList.push(...repoListTemp)
+            url_next = regexLinkNext.exec(response.headers.link)?.groups?.url_next
+        }
+
+        // console.log("repoList", repoList)
+        return repoList
     } catch (error) {
         console.error('Error making Octokit request:', error)
     }
@@ -87,7 +129,10 @@ export async function main() {
     console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     console.log('')
     const knownRepos = await listMYSRepos(octokit)
-    console.log('knownRepos', knownRepos)
+    console.log(
+        'knownRepos',
+        knownRepos.map((item) => `${item.name}:${item.clone_url}`),
+    )
     console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     console.log('')
     console.log('')
@@ -97,20 +142,26 @@ export async function main() {
     for (const repoData of reposData) {
         if (knownRepos.some((repo) => repo.name == repoData.repoName)) {
             console.log(`skipping '${repoData.repoName}'. already known..`)
-            repoData.clone_url = knownRepos.find((repo) => repo.name == repoData.repoName)
+            repoData.clone_url = knownRepos.find((repo) => repo.name == repoData.repoName).clone_url
         } else {
             console.log(`creating repo for '${repoData.repoName}'`)
+            repoData.description = `Bauteil des Make Your School Material Koffer Sets.`
             const result = await createRepoFromTemplate(
                 octokit,
                 repoData.repoName,
                 repoData.description,
             )
-            console.log(`result`, result)
+            const homepage = `https://make-your-school.github.io/mks-welcome/part/${repoData.repoName}`
+            repoSetHomepage(octokit, repoData.repoName, homepage)
+
+            // console.log(`result`, result)
             repoData.clone_url = result.data.clone_url
+            console.log(`    clone_url: ${repoData.clone_url}`)
+            console.log(`    ratelimit remaining: ${result.headers['x-ratelimit-remaining']}`)
         }
     }
     // save to disc
-    const repoDataJson = JSON.stringify(repoData, null, 4)
+    const repoDataJson = JSON.stringify(reposData, null, 4)
     fs.writeFileSync(reposDataFile, repoDataJson)
     console.log('done.')
 }
