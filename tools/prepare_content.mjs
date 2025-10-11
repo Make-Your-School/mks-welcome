551cdd3e819e46331ca868989eec219b9697b68e
s-light <git@s-light.eu>
2025-10-11 16:46:39 +0200

    prepare tooltips

--- tools/prepare_content.mjs
+++ tools/prepare_content.mjs
@@ -14,41 +14,78 @@ function writeAbbrFile(key, content) {
 }
 
 const regexToolTip =
-    /.*?\\\[simple\\_tooltip content='(?<tooltip_content>.*?)'\\\](?<tooltip_key>.*)\\\[\/simple\\_tooltip\\\].*/gms
+    /\\\[simple\\_tooltip content='(?<tooltip_content>.*?)'\\\](?<tooltip_key>.*?)\\\[\/simple\\_tooltip\\\]/gmsu
+
 async function extractToolTips(repoData, tooltipList) {
     console.log(`extract tooltips from '${repoData.repo_name}'...`)
-    const result = regexToolTip.exec(repoData.content_description)
-    if (result) {
-        const { tooltip_key, tooltip_content } = result.groups
-        if (tooltip_key && tooltip_content) {
-            console.log(`    found '${tooltip_key}'`)
-            if (tooltipList[tooltip_key]) {
-                // console.log(`    already available. checking content..`);
-                // const diffs = diff(tooltipList[tooltip_key], tooltip_content)
-                // if (diffs.length > 0) {
-                //     console.log(
-                //         `    already available. content differs. generating new key please check yourself.....`,
-                //     )
-                // }
-                if (tooltipList[tooltip_key] != tooltip_content) {
-                    console.log(`    already available. content differs.`)
-                    let tooltip_key_new = tooltip_key
-                    while (Object.keys(tooltipList).includes(tooltip_key_new)) {
-                        tooltip_key_new = tooltip_key_new + '-'
+
+    repoData.content_description.replaceAll(
+        regexToolTip,
+        (match, p1, p2, offset, string, groups) => {
+            const { tooltip_key, tooltip_content } = groups
+            if (tooltip_key && tooltip_content) {
+                console.log(`    found '${tooltip_key}'`)
+                if (tooltipList[tooltip_key]) {
+                    // console.log(`    already available. checking content..`);
+                    // const diffs = diff(tooltipList[tooltip_key], tooltip_content)
+                    // if (diffs.length > 0) {
+                    //     console.log(
+                    //         `    already available. content differs. generating new key please check yourself.....`,
+                    //     )
+                    // }
+                    if (tooltipList[tooltip_key] != tooltip_content) {
+                        console.log(`    already available. content differs.`)
+                        let tooltip_key_new = tooltip_key
+                        while (Object.keys(tooltipList).includes(tooltip_key_new)) {
+                            tooltip_key_new = tooltip_key_new + '-'
+                        }
+                        console.log(`     generated new key '${tooltip_key_new}'.`)
+                        console.log(`     please check content yourself.....`)
+                        tooltipList[tooltip_key_new] = tooltip_content
                     }
-                    console.log(`     generated new key '${tooltip_key_new}'.`)
-                    console.log(`     please check content yourself.....`)
-                    tooltipList[tooltip_key_new] = tooltip_content
+                } else {
+                    tooltipList[tooltip_key] = tooltip_content
                 }
             } else {
-                tooltipList[tooltip_key] = tooltip_content
+                console.log(
+                    `    extraction failed: key '${tooltip_key}', content '${tooltip_content}'`,
+                )
             }
-        } else {
-            console.log(`    extraction failed: key '${tooltip_key}', content '${tooltip_content}'`)
-        }
-    } else {
-        console.log(`    no tooltips found.`)
-    }
+            return tooltip_key
+        },
+    )
+    // const result = regexToolTip.exec(repoData.content_description)
+    // if (result) {
+    //     const { tooltip_key, tooltip_content } = result.groups
+    //     if (tooltip_key && tooltip_content) {
+    //         console.log(`    found '${tooltip_key}'`)
+    //         if (tooltipList[tooltip_key]) {
+    //             // console.log(`    already available. checking content..`);
+    //             // const diffs = diff(tooltipList[tooltip_key], tooltip_content)
+    //             // if (diffs.length > 0) {
+    //             //     console.log(
+    //             //         `    already available. content differs. generating new key please check yourself.....`,
+    //             //     )
+    //             // }
+    //             if (tooltipList[tooltip_key] != tooltip_content) {
+    //                 console.log(`    already available. content differs.`)
+    //                 let tooltip_key_new = tooltip_key
+    //                 while (Object.keys(tooltipList).includes(tooltip_key_new)) {
+    //                     tooltip_key_new = tooltip_key_new + '-'
+    //                 }
+    //                 console.log(`     generated new key '${tooltip_key_new}'.`)
+    //                 console.log(`     please check content yourself.....`)
+    //                 tooltipList[tooltip_key_new] = tooltip_content
+    //             }
+    //         } else {
+    //             tooltipList[tooltip_key] = tooltip_content
+    //         }
+    //     } else {
+    //         console.log(`    extraction failed: key '${tooltip_key}', content '${tooltip_content}'`)
+    //     }
+    // } else {
+    //     console.log(`    no tooltips found.`)
+    // }
 }
 
 // async function prepareContentInfolist(repoData) {
@@ -68,8 +105,8 @@ export async function main() {
     const tooltipListJSON = JSON.stringify(tooltipList, null, 4)
     fs.writeFileSync('tooltipList.json', tooltipListJSON)
     console.log(`'tooltipList.json' saved to disk.`)
-    for (const {key, content} of Object.entries(tooltipList)) {
-        writeAbbrFile(key, content)
+    for (const { key, content } of Object.entries(tooltipList)) {
+        // writeAbbrFile(key, content)
     }
 }
 
