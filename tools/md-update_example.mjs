2e1bdff6efd73baf694a82bdf39621f2f37da203
s-light <git@s-light.eu>
2025-10-29 01:29:30 +0100

    bulk-edit examples

--- tools/md-update_example.mjs
+++ tools/md-update_example.mjs
@@ -7,11 +7,10 @@ import path from 'path'
 import { getsubmodules } from '../getsubmodules.js'
 
 let exampleSectionContent = '\n'
-    exampleSectionContent += '## Beispiele\n'
-    exampleSectionContent += '\n'
-    exampleSectionContent += '!!!show-examples:./examples/\n'
-    exampleSectionContent += '\n'
-
+exampleSectionContent += '## Beispiele\n'
+exampleSectionContent += '\n'
+exampleSectionContent += '!!!show-examples:./examples/\n'
+exampleSectionContent += '\n'
 
 async function updateExampleSection(submodule) {
     // console.log('submodule', submodule)
@@ -47,16 +46,18 @@ async function updateNoValidExampleSection(submodule) {
     }
 }
 
-async function checkMissingExampleSection(submodule) {
-    // console.log('submodule', submodule)
-    const filepath = path.join(submodule.path, 'readme.md')
-    let readmeContent = fs.readFileSync(filepath, 'utf8')
+async function checkMissingExampleSections(submodules) {
+    console.log('Example Section Missing:')
+    for (const [submodule_name, submodule] of Object.entries(submodules)) {
+        const filepath = path.join(submodule.path, 'readme.md')
+        let readmeContent = fs.readFileSync(filepath, 'utf8')
 
-    if (readmeContent.find(/^## Beispiel.*?$/gm)) {
-        // console.log('   skipping. no changes in readme.')
-    } else {
-        fs.writeFileSync(filepath, readmeContentTweaked)
-        console.log('    Example Section Missing!')
+        const checkRegEx = /^## Beispiel.*?$/gm
+        if (checkRegEx.test(readmeContent)) {
+            // console.log('   fine')
+        } else {
+            console.log(`    '${submodule_name}'`)
+        }
     }
 }
 
@@ -66,12 +67,12 @@ export async function main() {
     // console.log(`found '${reposData.length}' repos to process..`)
     // console.log(`found '${reposData.length}' repos to process..`)
     // // const repoData = reposData[5]; {
-    for (const [submodule_name, submodule] of Object.entries(submodules)) {
-        console.log(`submodule_name '${submodule_name}'`)
-        await updateExampleSection(submodule)
-        // await updateNoValidExampleSection(submodule)
-        // await checkMissingExampleSection(submodule)
-    }
+    // for (const [submodule_name, submodule] of Object.entries(submodules)) {
+    //     console.log(`submodule_name '${submodule_name}'`)
+    // await updateExampleSection(submodule)
+    //     // await updateNoValidExampleSection(submodule)
+    // }
+    checkMissingExampleSections(submodules)
 }
 
 main()
