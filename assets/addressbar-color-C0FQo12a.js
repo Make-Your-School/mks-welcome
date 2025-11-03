f642785b948e1f5b15b70915ba220a0cfb914d27
s-light <git@s-light.eu>
2025-11-03 22:18:08 +0000

    Deploying to gh-pages from @ Make-Your-School/mks-welcome@c94e55f7d71a1e9a00a12092da581ab6c280f894 🚀

--- assets/addressbar-color-C0FQo12a.js
+++
@@ -1,7 +0,0 @@
-import { k as boot, A as AddressbarColor } from "./index-CXu0g4MB.js";
-const addressbarColor = boot(() => {
-  AddressbarColor.set("#a2e3fa");
-});
-export {
-  addressbarColor as default
-};
