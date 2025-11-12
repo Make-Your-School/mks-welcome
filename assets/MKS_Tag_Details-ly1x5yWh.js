2f2997489494a376d3865cb34aaf14836fc43df5
s-light <git@s-light.eu>
2025-11-12 22:02:48 +0000

    Deploying to gh-pages from @ Make-Your-School/mks-welcome@d7df1ebbc5e793f81f8869d2ced5432634880bc1 🚀

---
+++ assets/MKS_Tag_Details-ly1x5yWh.js
@@ -0,0 +1,32 @@
+import { Q as QPage } from "./QPage-CVFPm2LW.js";
+import { H as createBlock, v as openBlock, P as withCtx, j as createVNode, ah as resolveDynamicComponent, n as unref } from "./index-CaQ9iIZX.js";
+import { T as TagBauteilListe } from "./TagBauteilListe-MugPR_pu.js";
+import { u as useMDContentStore } from "./mdContent-CS6ME5PX.js";
+import "./_plugin-vue_export-helper-1tPrXgE0.js";
+import "./readme-CR1rdx8y.js";
+import "./about-BIR-VYe5.js";
+const _sfc_main = {
+  __name: "MKS_Tag_Details",
+  props: {
+    tag_name: String
+  },
+  setup(__props) {
+    const props = __props;
+    const mdContent = useMDContentStore();
+    console.log("mdContent", mdContent);
+    const tag = mdContent.mks.tags[props.tag_name];
+    console.log("tag", tag);
+    return (_ctx, _cache) => {
+      return openBlock(), createBlock(QPage, null, {
+        default: withCtx(() => [
+          (openBlock(), createBlock(resolveDynamicComponent(unref(tag).content))),
+          createVNode(TagBauteilListe, { tag: unref(tag) }, null, 8, ["tag"])
+        ]),
+        _: 1
+      });
+    };
+  }
+};
+export {
+  _sfc_main as default
+};
