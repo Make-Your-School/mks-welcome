39266ac6cca41929682c1e2a9ddb62fbebf842ae
s-light <git@s-light.eu>
2025-11-12 20:24:08 +0000

    Deploying to gh-pages from @ Make-Your-School/mks-welcome@17e20763f5aa93246acc950039267f1f16efdb60 🚀

---
+++ assets/use-dark-rLIRaZpG.js
@@ -0,0 +1,14 @@
+import { c as computed } from "./index-Beul2XkX.js";
+const useDarkProps = {
+  dark: {
+    type: Boolean,
+    default: null
+  }
+};
+function useDark(props, $q) {
+  return computed(() => props.dark === null ? $q.dark.isActive : props.dark);
+}
+export {
+  useDark as a,
+  useDarkProps as u
+};
