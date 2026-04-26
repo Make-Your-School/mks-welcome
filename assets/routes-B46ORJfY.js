7f65308f864a12f52e2798ae6341ba1b611b24c8
s-light <git@s-light.eu>
2026-04-26 21:49:12 +0000

    Deploying to gh-pages from @ Make-Your-School/mks-welcome@668250292d6b2b22603b4ab0f34b69e8006a1134 🚀

--- assets/routes-B46ORJfY.js
+++ assets/routes-B46ORJfY.js
@@ -1,4 +1,4 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/MKS_Parts-BdFtjKmv.js","assets/_plugin-vue_export-helper-Dl-_tC7C.js","assets/vue.esm-bundler-DkjeOeAQ.js","assets/chunk-C0u3WHrm.js","assets/dist-u9SxfPhV.js","assets/QBtn-DZW7z61w.js","assets/QIcon-BIe_yU5L.js","assets/render-AG-3FfQy.js","assets/inject-obj-prop-B-maADS4.js","assets/QSpinner-C9TaWt0d.js","assets/use-router-link-CkuTaxqL.js","assets/Ripple-cZik0Z5F.js","assets/event-JAmtdkur.js","assets/QCard-BrwQdTtb.js","assets/use-dark-D7HPSSY-.js","assets/QCardSection-BpCocpXo.js","assets/QInput-BG03k-Fb.js","assets/use-key-composition-Czd8xtjr.js","assets/private.use-form-DoNiTCW_.js","assets/use-id-DWrbHNzg.js","assets/Platform-Hxr0V5Kb.js","assets/debounce-xC9d6Tps.js","assets/symbols-CTfU4lpL.js","assets/QPage-HPiQh4dS.js","assets/vue-router-C1WeEvMh.js","assets/mdContent-Bl-UT33K.js","assets/pinia-BNAaAcy4.js","assets/about-Bh6u_n79.js","assets/readme-DJ5bbZji.js","assets/mdContent-BuYODJz4.css","assets/MKS_Parts-BMTm1jcY.css","assets/MKS_Tags-Bek6laO8.js","assets/TagBauteilListe-3-tWFmyW.js","assets/TagBauteilListe-tn0RQdqM.css","assets/MKS_Tags-Byav8agq.css","assets/MKS_Abbr-Durh_uop.js","assets/QExpansionItem-CY57Wo8b.js","assets/QItemLabel-D9M7YRL7.js","assets/QSeparator-Bf3NzGLA.js","assets/QList-DdZIs395.js","assets/AboutPage-Dck5od3K.js","assets/SettingsPage-1wS28QxS.js","assets/use-checkbox-CqeGl3e8.js","assets/use-timeout-B36y8WMi.js","assets/nodes-BpIwPEsi.js","assets/QSelect-Dd9iDzsc.js","assets/format-DLB5G-Pz.js","assets/History-dJyuw0sM.js","assets/MDDemo-eGcpdCCQ.js","assets/MDDev--B6PX2iF.js","assets/parts-meta-DnNBruko.js","assets/QCheckbox-Cngqk9Fj.js","assets/use-quasar-DjkpUp09.js","assets/parts-meta-B7HlzVRC.css","assets/MainLayout-DCOCXBOe.js","assets/touch-DSqVpVWs.js","assets/MainLayout-Bnp2Cdoc.css","assets/MKS_Part_Details-CV3gOTYG.js","assets/MKS_Part_Details-7iw0FdPi.css","assets/MKS_Part_SubMarkdownFile-oSnauumw.js","assets/MyMarkdown-CI4JPIWU.js","assets/es-rhvVhq1b.js","assets/MKS_Part_SubMarkdownFile-CjzMJJeF.css","assets/MKS_Tag_Details-B3pe79AL.js","assets/ErrorNotFound-DtIIzC5t.js"])))=>i.map(i=>d[i]);
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/MKS_Parts-BdFtjKmv.js","assets/_plugin-vue_export-helper-Dl-_tC7C.js","assets/vue.esm-bundler-DkjeOeAQ.js","assets/chunk-C0u3WHrm.js","assets/dist-u9SxfPhV.js","assets/QBtn-DZW7z61w.js","assets/QIcon-BIe_yU5L.js","assets/render-AG-3FfQy.js","assets/inject-obj-prop-B-maADS4.js","assets/QSpinner-C9TaWt0d.js","assets/use-router-link-CkuTaxqL.js","assets/Ripple-cZik0Z5F.js","assets/event-JAmtdkur.js","assets/QCard-BrwQdTtb.js","assets/use-dark-D7HPSSY-.js","assets/QCardSection-BpCocpXo.js","assets/QInput-BG03k-Fb.js","assets/use-key-composition-Czd8xtjr.js","assets/private.use-form-DoNiTCW_.js","assets/use-id-DWrbHNzg.js","assets/Platform-Hxr0V5Kb.js","assets/debounce-xC9d6Tps.js","assets/symbols-CTfU4lpL.js","assets/QPage-HPiQh4dS.js","assets/vue-router-C1WeEvMh.js","assets/mdContent-Bl-UT33K.js","assets/pinia-BNAaAcy4.js","assets/about-Bh6u_n79.js","assets/readme-DJ5bbZji.js","assets/mdContent-BuYODJz4.css","assets/MKS_Parts-BMTm1jcY.css","assets/MKS_Tags-Bek6laO8.js","assets/TagBauteilListe-3-tWFmyW.js","assets/TagBauteilListe-tn0RQdqM.css","assets/MKS_Tags-Byav8agq.css","assets/MKS_Abbr-Durh_uop.js","assets/QExpansionItem-CY57Wo8b.js","assets/QItemLabel-D9M7YRL7.js","assets/QSeparator-Bf3NzGLA.js","assets/QList-DdZIs395.js","assets/AboutPage-Dck5od3K.js","assets/SettingsPage-1wS28QxS.js","assets/use-checkbox-CqeGl3e8.js","assets/use-timeout-B36y8WMi.js","assets/nodes-BpIwPEsi.js","assets/QSelect-Dd9iDzsc.js","assets/format-DLB5G-Pz.js","assets/History-dJyuw0sM.js","assets/MDDemo-eGcpdCCQ.js","assets/MDDev--B6PX2iF.js","assets/parts-meta-DnNBruko.js","assets/QCheckbox-Cngqk9Fj.js","assets/use-quasar-DjkpUp09.js","assets/parts-meta-B7HlzVRC.css","assets/MainLayout-DlSdRf32.js","assets/touch-DSqVpVWs.js","assets/MainLayout-Bnp2Cdoc.css","assets/MKS_Part_Details-CV3gOTYG.js","assets/MKS_Part_Details-7iw0FdPi.css","assets/MKS_Part_SubMarkdownFile-oSnauumw.js","assets/MyMarkdown-CI4JPIWU.js","assets/es-rhvVhq1b.js","assets/MKS_Part_SubMarkdownFile-CjzMJJeF.css","assets/MKS_Tag_Details-B3pe79AL.js","assets/ErrorNotFound-DtIIzC5t.js"])))=>i.map(i=>d[i]);
 import { t as __vitePreload } from "./preload-helper-Chyirnlg.js";
 //#region src/router/routes.js
 var childrenCommon = [
@@ -59,7 +59,7 @@ var childrenCommon = [
 ];
 var routes = [{
 	path: "/",
-	component: () => __vitePreload(() => import("./MainLayout-DCOCXBOe.js"), __vite__mapDeps([54,1,2,3,4,5,6,7,8,9,10,11,12,37,14,39,55,20,46,47,52,43,22,56])),
+	component: () => __vitePreload(() => import("./MainLayout-DlSdRf32.js"), __vite__mapDeps([54,1,2,3,4,5,6,7,8,9,10,11,12,37,14,39,55,20,46,47,52,43,22,56])),
 	children: [
 		{
 			path: "part/:part_name/",
@@ -79,6 +79,14 @@ var routes = [{
 			path: "tag/:tag_name?",
 			component: () => __vitePreload(() => import("./MKS_Tag_Details-B3pe79AL.js"), __vite__mapDeps([63,2,3,4,23,7,8,22,25,26,27,28,29,32,1,33])),
 			props: true
+		},
+		{
+			path: "mks-welcome/:subPath*",
+			redirect: (to) => {
+				console.log("to", to);
+				to.path = to.path.replace("mks-welcome/", "");
+				return to;
+			}
 		},
 		...childrenCommon
 	]
